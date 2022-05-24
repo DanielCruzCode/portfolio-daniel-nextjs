d93b76e141bd9416c58b07f570fdcabf18aaf2cb
Daniel Cruz <danielcruz@blacksip.com>
2022-05-23 21:05:25 -0500

    perf(next configs pages): change HEAD component to get better font resources

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,25 +1,11 @@
 // import '../styles/globals.css' // this project don't uses a CSS modules
 import type { AppProps } from "next/app";
-import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="true"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Sen:wght@400;700;800&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
       <Component {...pageProps} />
     </>
   );
 }
-
 export default MyApp;
